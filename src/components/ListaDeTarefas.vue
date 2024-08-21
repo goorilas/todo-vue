e94f7b52ce6bb1f849857709196b8ee62357a2de
goorilas <wdxz_100@hotmail.com>
2024-08-21 06:59:39 -0300

    colocando condição de quando não áh tarefas

--- src/components/ListaDeTarefas.vue
+++ src/components/ListaDeTarefas.vue
@@ -3,14 +3,17 @@ const props = defineProps(['tarefas']);
 </script>
 
 <template>
-    <ul class="list-group mt-4">
-        <li class="list-group-item" v-for="tarefa in props.tarefas">
-            <input @change="evento => tarefa.finalizada = evento.target.checked" :checked="tarefa.finalizada"
-                :id="tarefa.titulo" type="checkbox">
-            <label :class="{ done: tarefa.finalizada }" class="ms-3" :for="tarefa.titulo">
-                {{ tarefa.titulo }}
-            </label>
-        </li>
+    <ul class="list-group mt-4" >
+        <div v-if="tarefas != 0">
+            <li class="list-group-item" v-for="tarefa in props.tarefas">
+                <input @change="evento => tarefa.finalizada = evento.target.checked" :checked="tarefa.finalizada"
+                    :id="tarefa.titulo" type="checkbox">
+                <label :class="{ done: tarefa.finalizada }" class="ms-3" :for="tarefa.titulo">
+                    {{ tarefa.titulo }}
+                </label>
+            </li>
+        </div>
+        <h1 v-else>Não áh tarefas disponiveis</h1>
     </ul>
 </template>
 
